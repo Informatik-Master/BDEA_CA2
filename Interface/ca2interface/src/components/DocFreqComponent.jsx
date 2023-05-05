189cf72c2044a79e132ad505c5119c410ccd097a
Matthias Vonend <matthias.vonend@outlook.de>
2023-05-05 19:19:55 +0200

    fixes frequency result behaviour
    Co-authored-by:  Sofia911 <sofia.henninger@gmail.com>

--- Interface/ca2interface/src/components/DocFreqComponent.jsx
+++ Interface/ca2interface/src/components/DocFreqComponent.jsx
@@ -5,14 +5,17 @@ import DocFreqBatchComponent from './DocFreqBatchComponent';
 function DocFreqComponent() {
   const [word, setWord] = useState(''); // word: input value
   const [docFreq, setDocFreq] = useState(0); // docFreq: output value
+  const [resultIsReady, setResultIsReady] = useState(false);
 
   const handleInputChange = (event) => {  
     setWord(event.target.value.toUpperCase());
+    setResultIsReady(false);
   };
 
   const handleButtonClick = async () => { 
     FileService.getDocFrequency(word).then((response) => {
-      setDocFreq(response.data.frequency);
+      setDocFreq(response.data.frequency || 0);
+      setResultIsReady(true);
       }); 
     };
 
@@ -22,8 +25,8 @@ function DocFreqComponent() {
       <br></br>
       <input type="text" value={word} onChange={handleInputChange} />
       <button onClick={handleButtonClick}>Refresh Document Frequency</button>
-          {docFreq == undefined && <p>The word "{word}" does not appear in any documents.</p>}
-          {docFreq > 0 && <p>The word "{word}" appears in {docFreq} document(s).</p>}
+          {resultIsReady && docFreq === 0 && <p>The word "{word}" does not appear in any documents.</p>}
+          {resultIsReady && docFreq > 0 && <p>The word "{word}" appears in {docFreq} document(s).</p>}
     </div>
   );
 }
